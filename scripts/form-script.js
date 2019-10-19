b536b7407d7e5eb8c02f385b3e9ba2b99238254c
Макс <financial_monster@mail.ru>
2019-10-19 17:33:03 +0400

    Fixed popup and radio-button bugs, finished footer-map realisation

--- scripts/form-script.js
+++ scripts/form-script.js
@@ -7,27 +7,31 @@ var requiredSurname = document.querySelector(".form__sur-name");
 var requiredFirstname = document.querySelector(".form__first-name");
 var requiredEmail = document.querySelector(".form__email");
 
-submitButton.addEventListener("click", function(evt) {
+submitButton.addEventListener("click", function(evt) { 
     evt.preventDefault();
     if (!requiredSurname.value || !requiredFirstname.value || !requiredEmail.value) {
-        popupFailure.classList.add("show-js");     
+        popupFailure.classList.add("show-form-js");     
     } else {
-        popupSuccess.classList.add("show-js"); 
+        popupSuccess.classList.add("show-form-js"); 
     }
+    submitButton.classList.add("disabled-js");
 }); 
 
 successButton.addEventListener("click", function() {
-    popupSuccess.classList.remove("show-js"); 
+    popupSuccess.classList.remove("show-form-js"); 
+    submitButton.classList.remove("disabled-js");
 });
 
 failureButton.addEventListener("click", function() {
-    popupFailure.classList.remove("show-js"); 
+    popupFailure.classList.remove("show-form-js");
+    submitButton.classList.remove("disabled-js"); 
 });
 
 window.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27 && (popupSuccess.classList.contains("show-js") || popupFailure.classList.contains("show-js"))) {
+    if (evt.keyCode === 27 && (popupSuccess.classList.contains("show-form-js") || popupFailure.classList.contains("show-js"))) {
         evt.preventDefault();
-        popupSuccess.classList.remove("show-js"); 
-        popupFailure.classList.remove("show-js"); 
+        popupSuccess.classList.remove("show-form-js"); 
+        popupFailure.classList.remove("show-form-js"); 
+        submitButton.classList.remove("disabled-js");
     }
 });
--- scripts/script.js
+++ scripts/script.js
@@ -5,14 +5,22 @@ var pageHeader = document.querySelector(".page-header");
 
 menuOpen.addEventListener("click", function (evt) {
     evt.preventDefault();
-    menu.classList.add("show-js");
-    menuClose.classList.add("show-js");
-    pageHeader.classList.add("margintop-js");
+    if (menu.classList.contains("hide-js")) {
+        menu.classList.add("show-js");
+        menuClose.classList.add("show-js");
+        pageHeader.classList.add("margintopdown-js");
+    } 
 });
 
 menuClose.addEventListener("click", function (evt) {
     evt.preventDefault();
-    menu.classList.remove("show-js");
-    menuClose.classList.remove("show-js");
-    pageHeader.classList.remove("margintop-js");
+    if (menu.classList.contains("show-js")) {
+        menu.classList.remove("show-js");
+        menuClose.classList.remove("show-js");
+        pageHeader.classList.remove("margintopdown-js"); 
+    } else {
+        menu.classList.add("hide-js");
+        menuClose.classList.add("hide-js");
+        pageHeader.classList.add("margintopup-js");
+    }
 });
